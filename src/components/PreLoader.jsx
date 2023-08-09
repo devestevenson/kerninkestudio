ba78b278eaa70840d1dbc4f474f25b2e9112d36c
devestevenson <devestevenson@gmail.com>
2023-08-09 12:51:06 -0500

    cambio color de texto

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -2,7 +2,7 @@
 const Preloader = () => {
     return (
       <div className="w-full h-screen bg-cWhite flex justify-center items-center">
-        <h2 data-aos="fade-up" className='text-kerninkGrayDark text-3xl font-poppinsBold'>
+        <h2 data-aos="fade-up" className='text-cGrayDark text-3xl font-poppinsBold'>
             KERNINK © 2023
         </h2>
       </div>
